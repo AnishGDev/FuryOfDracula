494e43571fedc278027ad10fa57ad8bbf84a7078
Anish Ghai <anishgdev@gmail.com>
2020-07-30 08:36:15 +1000

    A lot of minimax stuff implemented. Still not ready or tested

--- hunt/DraculaView.c
+++ hunt/DraculaView.c
@@ -13,40 +13,69 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "DraculaView.h"
 #include "Game.h"
 #include "GameView.h"
 #include "Map.h"
-// add your own #includes here
 
-static bool hiddenInLast5 (DraculaView dv);
-static bool DoubleInLast5 (DraculaView dv);
-static PlaceId *ReplaceWithDoubleBack (PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs);
-static PlaceId *RemoveDoubleBack (PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs);
+static bool hiddenInLast5(DraculaView dv, PlaceId *moveHist, int numHistMoves);
+static bool doubleInLast5(DraculaView dv, PlaceId *moveHist, int numHistMoves);
+static PlaceId *ReplaceWithDoubleBack(
+	PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs
+);
+static PlaceId *RemoveDoubleBack(
+	PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs
+);
+// Cache trail info to reduce speed for repeated calls. 
+struct _trailInfo {
+	PlaceId * trail;
+	int trailLen;
+	bool isRealTrail; 
+	bool canFree;
+};
 
-// TODO: ADD YOUR OWN STRUCTS HERE
-
+// The basic idea is to mirror the gameView struct but allow the AI
+// to modify it so it can store future versions of this game and evaluate them.
+// This ensures that the main gameView itself is not modified so we can get original information 
+// from it
 struct draculaView {
 	GameView gv;
+	Trail trailInfo;
+	// Internal representation which can be modified to get a theoretical state.
+	// This allows dracula AI to modify states and explore options without modifying actual state.
+	PlaceId whereIsPlayer[NUM_PLAYERS]; // idk why but whereIsPlayer[i] is not working.
+	Round round; // internal storage of round.
 };
 
 ////////////////////////////////////////////////////////////////////////
 // Constructor/Destructor
 
-DraculaView DvNew(char *pastPlays, Message messages[])
-{
-	DraculaView new = malloc(sizeof(DraculaView));
+DraculaView DvNew(char *pastPlays, Message messages[]) {
+	DraculaView new = malloc(sizeof(struct draculaView));
+
 	if (new == NULL) {
 		fprintf(stderr, "Couldn't allocate DraculaView\n");
 		exit(EXIT_FAILURE);
 	}
 	new->gv = GvNew(pastPlays,messages);
+	new->trailInfo = malloc(sizeof(struct _trailInfo));
+	new->trailInfo->trail = GvGetLastMoves(new->gv, PLAYER_DRACULA, 
+							TRAIL_SIZE-1, &(new->trailInfo->trailLen), &(new->trailInfo->canFree));
+							
+	//new->whereIsPlayer = malloc(sizeof(enum placeId) * NUM_PLAYERS); 
+	for (int i =0; i < NUM_PLAYERS; i++) {
+		new->whereIsPlayer[i] = GvGetPlayerLocation(new->gv, i);
+	}
+	new->round = GvGetRound(new->gv);
+	//new->isRealTrail = true;
 	return new;
 }
 
-void DvFree(DraculaView dv)
-{
+void DvFree(DraculaView dv) {
+	// getLocation functions handles the freeing for trail. The struct simply stores it. 
+	//free(dv->trailInfo);
+	//free(dv->whereIsPlayer);
 	GvFree(dv->gv);
 	free(dv);
 }
@@ -54,42 +83,35 @@ void DvFree(DraculaView dv)
 ////////////////////////////////////////////////////////////////////////
 // Game State Information
 
-Round DvGetRound(DraculaView dv)
-{
+Round DvGetRound(DraculaView dv) {
 	return GvGetRound(dv->gv);
 }
 
-int DvGetScore(DraculaView dv)
-{
+int DvGetScore(DraculaView dv) {
 	return GvGetScore(dv->gv);
 }
 
-int DvGetHealth(DraculaView dv, Player player)
-{
+int DvGetHealth(DraculaView dv, Player player) {
 	return GvGetHealth(dv->gv, player);
 }
 
-PlaceId DvGetPlayerLocation(DraculaView dv, Player player)
-{
+PlaceId DvGetPlayerLocation(DraculaView dv, Player player) {
 	return GvGetPlayerLocation(dv->gv, player);
 }
 
-PlaceId DvGetVampireLocation(DraculaView dv)
-{
+PlaceId DvGetVampireLocation(DraculaView dv) {
 	return GvGetVampireLocation(dv->gv);
 }
 
-PlaceId *DvGetTrapLocations(DraculaView dv, int *numTraps)
-{
+PlaceId *DvGetTrapLocations(DraculaView dv, int *numTraps) {
 	return GvGetTrapLocations(dv->gv, numTraps);
 }
 
 ////////////////////////////////////////////////////////////////////////
 // Making a Move
 
-PlaceId *DvGetValidMoves(DraculaView dv, int *numReturnedMoves)
-{
-	//Checking if dracula has made a move
+PlaceId *DvGetValidMoves(DraculaView dv, int *numReturnedMoves) {
+	// Check if dracula has made a move
 	if (GvGetPlayerLocation(dv->gv, PLAYER_DRACULA) == NOWHERE) {
 		*numReturnedMoves = 0;
 		return NULL;
@@ -97,138 +119,181 @@ PlaceId *DvGetValidMoves(DraculaView dv, int *numReturnedMoves)
 
 	PlaceId *moves = DvWhereCanIGoByType(dv, true, true, numReturnedMoves);
 
-	bool doubleBackInTrail = DoubleInLast5(dv);
 
 	int numHistMoves;
 	bool canFree = false;
-	int numMoves = 5;
-	PlaceId *trailMoves = GvGetLastMoves(dv->gv, PLAYER_DRACULA, 
-		numMoves, &numHistMoves, &canFree);
+	//int numMoves = 5;
+	PlaceId * trailMoves = returnCurrentTrail(dv, &numHistMoves, &canFree);
+	/*
+	PlaceId *trailMoves = GvGetLastMoves(
+		dv->gv, PLAYER_DRACULA, numMoves, &numHistMoves, &canFree
+	);
+	*/
+	bool doubleBackInTrail = doubleInLast5(dv, trailMoves, numHistMoves);
 	
-	if (!doubleBackInTrail) {	//Add Double backs as moves
+	if (!doubleBackInTrail) {	// Add Double backs as moves
 		moves = ReplaceWithDoubleBack(moves, trailMoves, numHistMoves, numReturnedMoves);
 	} else {
 		RemoveDoubleBack(moves, trailMoves, numHistMoves, numReturnedMoves);
 	}
 
-	bool hiddenInTrail = hiddenInLast5(dv);
+	bool hiddenInTrail = hiddenInLast5(dv, trailMoves, numHistMoves);
 
-	if (placeIdToType(DvGetPlayerLocation(dv, PLAYER_DRACULA)) != SEA) {	//Cant Hide at sea
-		if (!hiddenInTrail) {	//Add Hide as a move
+	if (placeIdToType(DvGetPlayerLocation(dv, PLAYER_DRACULA)) != SEA) { // Can't Hide at sea
+		if (!hiddenInTrail) { // Add Hide as a move
 			*numReturnedMoves += 1;
 			moves = realloc(moves, sizeof(PlaceId) * (*numReturnedMoves));
 			moves[*numReturnedMoves-1] = HIDE;
 		}
 	}
+	if (canFree) free(trailMoves);
 	return moves;
 }
+
+
+int calculateDistance(DraculaView dv, PlaceId from, PlaceId to) {
+	return 0;
+}
+
+void setTheoreticalState(DraculaView dv, PlaceId * trailToSet, Round round, PlaceId whereIsPlayer[]) {
+	// Set internal representation to what the Dracula AI wants to evaluate future states. 
+	dv->round = round;
+	memcpy(dv->whereIsPlayer, whereIsPlayer, sizeof(PlaceId) * NUM_PLAYERS);
+	memcpy(dv->trailInfo->trail, trailToSet, sizeof(PlaceId) * TRAIL_SIZE);
+	//dv->trailInfo->trail = trailToSet;
+}
+
+void resetTheoreticalState(DraculaView dv) {
+	dv->trailInfo->trail = GvGetLastMoves(dv->gv, PLAYER_DRACULA, 
+							TRAIL_SIZE-1, &(dv->trailInfo->trailLen), &(dv->trailInfo->canFree));
+}
+
+PlaceId * returnCurrentTrail(DraculaView dv, int *numReturnedLocs, bool * canFree) {
+	// Return cached trail locations. Can be used to change represent future states by AI. 
+	*numReturnedLocs = dv->trailInfo->trailLen;
+	*canFree = dv->trailInfo->canFree; 
+	return dv->trailInfo->trail;
+} 
+
+
 
 PlaceId *DvWhereCanIGo(DraculaView dv, int *numReturnedLocs)
 {
 	return DvWhereCanIGoByType(dv, true, true, numReturnedLocs);
 }
 
-PlaceId *DvWhereCanIGoByType(DraculaView dv, bool road, bool boat,
-                             int *numReturnedLocs)
-{
-	return DvWhereCanTheyGoByType(dv, PLAYER_DRACULA, road, false, boat, numReturnedLocs);
+PlaceId *DvWhereCanIGoByType(
+	DraculaView dv, bool road, bool boat, int *numReturnedLocs
+) {
+	return DvWhereCanTheyGoByType(
+		dv, PLAYER_DRACULA, road, false, boat, numReturnedLocs
+	);
 }
 
-PlaceId *DvWhereCanTheyGo(DraculaView dv, Player player,
-                          int *numReturnedLocs)
-{
+PlaceId *DvWhereCanTheyGo(
+	DraculaView dv, Player player, int *numReturnedLocs
+) {
 	if (player == PLAYER_DRACULA) {
-		return DvWhereCanTheyGoByType(dv, PLAYER_DRACULA, true, false, true, numReturnedLocs);
+		return DvWhereCanTheyGoByType(
+			dv, PLAYER_DRACULA, true, false, true, numReturnedLocs
+		);
 	} else {
-		return DvWhereCanTheyGoByType(dv, player, true, true, true, numReturnedLocs);
+		return DvWhereCanTheyGoByType(
+			dv, player, true, true, true, numReturnedLocs
+		);
 	}
 }
 
-PlaceId *DvWhereCanTheyGoByType(DraculaView dv, Player player,
-                                bool road, bool rail, bool boat,
-                                int *numReturnedLocs)
-{
-	//Checking if player has made a move
+// Subset of Gv such that we can modify individual player locations
+// This allows us to set "future states" of the game and evaluate them in the AI. 
+
+
+PlaceId *DvWhereCanTheyGoByType(
+	DraculaView dv, Player player,
+	bool road, bool rail, bool boat,
+	int *numReturnedLocs
+) {
+	// Check if player has made a move
 	if (GvGetPlayerLocation(dv->gv, player) == NOWHERE) {
 		*numReturnedLocs = 0;
 		return NULL;
 	}
 
 	if (player == PLAYER_DRACULA) {
+		PlaceId *places = GvGetReachableByType(dv->gv, player, dv->round, dv->whereIsPlayer[PLAYER_DRACULA], road, rail, boat, numReturnedLocs);
+		/*
 		PlaceId *places = GvGetReachableByType(dv->gv, player, DvGetRound(dv),
-											DvGetPlayerLocation(dv, player),
-											road, rail, boat, numReturnedLocs);
-		
-		//Account for trail Restrictions
-		bool doubleBackInTrail = DoubleInLast5(dv);
-
+												DvGetPlayerLocation(dv, player),road, 
+												rail, boat, numReturnedLocs);
+		*/
+		// Account for trail Restrictions
+	
 		int numHistMoves;
 		bool canFree = false;
-		int numMoves = 5;
-		PlaceId *trailMoves = GvGetLastMoves(dv->gv, PLAYER_DRACULA, 
-			numMoves, &numHistMoves, &canFree);
+		//int numMoves = 5;
+		PlaceId * trailMoves = returnCurrentTrail(dv, &numHistMoves, &canFree);
+		/*
+		PlaceId *trailMoves = GvGetLastMoves(
+			dv->gv, PLAYER_DRACULA, numMoves, &numHistMoves, &canFree
+		);
+		*/
+		bool doubleBackInTrail = doubleInLast5(dv, trailMoves, numHistMoves);
 
-		if (doubleBackInTrail) {	//Remove Double back locations
+		if (doubleBackInTrail) { // Remove Double back locations
 			places = RemoveDoubleBack(places, trailMoves, numHistMoves, numReturnedLocs);
 			
-			//If Can Still Hide Add current location Back
-			bool hiddenInTrail = hiddenInLast5(dv);
+			// If still can hide, add current location Back
+			bool hiddenInTrail = hiddenInLast5(dv, trailMoves, numHistMoves);
 			if (!hiddenInTrail) {
 				PlaceId currLoc = DvGetPlayerLocation(dv, PLAYER_DRACULA);
 				bool in = false;
+
 				for (int i = 0; i < *numReturnedLocs; i++) {
 					if (places[i] == currLoc) {
 						in = true;
 						break;
 					}
 				}
+
 				if (!in) {
 					*numReturnedLocs += 1;
 					places = realloc(places, sizeof(PlaceId) * (*numReturnedLocs));
-					places[*numReturnedLocs-1] = currLoc;
+					places[*numReturnedLocs - 1] = currLoc;
 				}
-
 			}
 		}
 
 		if (canFree) free(trailMoves);
 		return places;
 	} else {
-		return GvGetReachableByType(dv->gv, player, DvGetRound(dv),
-											DvGetPlayerLocation(dv, player),
-											road, rail, boat, numReturnedLocs);
+		return GvGetReachableByType(
+			dv->gv, player, dv->round,
+			dv->whereIsPlayer[player],
+			road, rail, boat,
+			numReturnedLocs
+		);
 	}
 }
 
-//Checks if dracula has used a Hide in the last 5 moves
-static bool hiddenInLast5 (DraculaView dv) {
-	bool canFree = false;
+// Checks if dracula has used a Hide in the last 5 moves
+static bool hiddenInLast5 (DraculaView dv, PlaceId *moveHist, int numHistMoves) {
 	bool found = false;
-	int numReturnedMoves;
 
-	PlaceId *moveHist = GvGetLastMoves (dv->gv, PLAYER_DRACULA, 
-		5, &numReturnedMoves, &canFree);
-
-	for (int i = 0; i < numReturnedMoves; i++) {
+	for (int i = 0; i < numHistMoves; i++) {
 		if (moveHist[i] == HIDE) {
 			found = true;
 			break;
 		}
 	}
-	if (canFree) free(moveHist);
+
 	return found;
 }
 
 //Checks if dracula has used a doubleback in the last 5 moves
-static bool DoubleInLast5 (DraculaView dv) {
-
-	bool canfree = false;
+static bool doubleInLast5 (DraculaView dv, PlaceId *moveHist, int numHistMoves) {
 	bool found = false;
-	int numReturnedMoves;
-	PlaceId *moveHist = GvGetLastMoves (dv->gv, PLAYER_DRACULA, 
-		5, &numReturnedMoves, &canfree);
-	
-	for (int i = 0; i < numReturnedMoves; i++) {
+
+	for (int i = 0; i < numHistMoves; i++) {
 		//IF move is DOUBLE_BACK_1 or 2 or 3 or 4 or 5
 		if (moveHist[i] >= DOUBLE_BACK_1 && moveHist[i] <= DOUBLE_BACK_5) {
 			found = true;
@@ -236,34 +301,43 @@ static bool DoubleInLast5 (DraculaView dv) {
 		}
 	}
 
-	if (canfree) free(moveHist);
 	return found;
 }
 
-//Replaces Items in locations with double backs, assumes double back isnt in trail
-static PlaceId *ReplaceWithDoubleBack (PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs) {
-	for (int i = 0; i < numHistMoves; i++) {	//Go through places and see if they occur in
-												//Dracula's trail, if they do, replace them with double_back_n
+// Replaces Items in locations with Double Backs; assumes Double Back isn't in trail
+static PlaceId *ReplaceWithDoubleBack(
+	PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs
+) {
+	// Go through places and see if they occur in Dracula's trail, if they do, replace
+	// them with double_back_n
+	for (int i = 0; i < numHistMoves; i++) {
 		for (int j = 0; j < *numReturnedLocs; j++) {
 			if (trailMoves[i] == locations[j]) {
-				locations[j] = DOUBLE_BACK_1 + i;//trailMoves[i]; //Move to somewere 1 moves ago - how many moves before it was
+				// Move to somewere 1 moves ago -- how many moves before it was
+				locations[j] = DOUBLE_BACK_1 + i; // trailMoves[i]; 
 			}
 		}
 	}
+
 	return locations;
 }
 
-static PlaceId *RemoveDoubleBack (PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs) {
+static PlaceId *RemoveDoubleBack(
+	PlaceId *locations, PlaceId *trailMoves, int numHistMoves, int *numReturnedLocs
+) {
 	PlaceId remove = -3;
-	for (int i = 0; i < numHistMoves; i++) {	//Go through places and see if they occur in
-												//Dracula's trail, if they do, remove them
+
+	// Go through places and see if they occur in Dracula's trail, if they do, remove them
+	for (int i = 0; i < numHistMoves; i++) {
 		for (int j = 0; j < *numReturnedLocs; j++) {
 			if (trailMoves[i] == locations[j]) {
-				locations[j] = remove; //Move to somewere 5 moves ago - how many moves before it was
+				// Move to somewere 5 moves ago - how many moves before it was
+				locations[j] = remove;
 			}
 		}
 	}
-	//Rearrange the array
+
+	// Rearrange the array
 
 	int numShift = 0;
 	for (int i = 0; i < *numReturnedLocs; i++) {
@@ -273,11 +347,14 @@ static PlaceId *RemoveDoubleBack (PlaceId *locations, PlaceId *trailMoves, int n
 			locations[i-numShift] = locations[i];
 		}
 	}
+
 	*numReturnedLocs -= numShift;
+	
 	locations = realloc(locations, sizeof(PlaceId) * (*numReturnedLocs));
 
 	return locations;
 }
+
 ////////////////////////////////////////////////////////////////////////
 // Your own interface functions
 
--- hunt/dracula.c
+++ hunt/dracula.c
@@ -12,16 +12,22 @@
 #include "dracula.h"
 #include "DraculaView.h"
 #include "Game.h"
+#include <limits.h>
 #include <assert.h> // for testing purposes. Remove later.
 // structure to store a snapshot of a game state. 
 #define MAX_DEPTH 3 // might increase depending on how long it takes to evaluate.
+#define INFINITY 1e9
+#define NEGATIVE_INFINITY -1e9
+
 
 typedef struct _state {
-	PlaceId id; // to keep track of which state. 
+	PlaceId whereIsPlayer[5]; // to keep track of which placeID. 5th one is dracula. 
 	// used in evalFunction. 
 	int health;
 	int score;
-	int distFromPlayers[NUM_PLAYERS]; // 5TH one could be Castle Drac with negative weight. 
+	PlaceId * trail;// Store current trail info. 
+	int distFromPlayers[NUM_PLAYERS];
+	// store CD later.
 } State; 
 
 static inline int max(int a, int b) {
@@ -37,7 +43,31 @@ int minimax(DraculaView dv, State currState, int currDepth, bool isMaximising);
 void decideDraculaMove(DraculaView dv)
 {
 	// TODO: Replace this with something better!
-	registerBestPlay("CD", "Mwahahahaha");
+	// Set up initial root state
+	int currScore = DvGetScore(dv);
+	if (currScore == GAME_START_SCORE) {
+		// Only first round. 
+		registerBestPlay("CD", "Mwahahahaha");
+	} else {
+		State * s = malloc(sizeof(struct _state)); 
+		s->whereIsPlayer[PLAYER_DRACULA] = DvGetPlayerLocation(dv, PLAYER_DRACULA);
+		for (int i =0; i < NUM_PLAYERS - 1; i++) {
+			s->whereIsPlayer[i] = DvGetPlayerLocation(dv, i); 
+			s->distFromPlayers[i] = calculateDistance(dv, s->whereIsPlayer[PLAYER_DRACULA], s->whereIsPlayer[i]); //calculateDistance(dv, from, to); 
+		}
+		// 5th element will represent distance to castle dracula.
+		s->distFromPlayers[PLAYER_DRACULA] = calculateDistance(dv, s->whereIsPlayer[PLAYER_DRACULA], CASTLE_DRACULA); 
+		s->health = DvGetHealth(dv, PLAYER_DRACULA);
+		s->score = currScore;
+		int numReturned = 0;
+		bool canFree = false; 
+		s->trail = returnCurrentTrail(dv, &numReturned, &canFree); 
+		registerBestPlay("CD", "Mwahahahaha");
+	}
+}
+
+State updateTrailInfo(State *s) {
+	return *s;
 }
 
 // Takes a game state s, and evaluates it based on how good is it.
@@ -45,53 +75,95 @@ int evalFunction(State s) {
 	return 0; 
 }
 // If Dracula was at place p, what would his distance to other players be?
-void evalDistFromPlayers(PlaceId p, State s) {
-
+void evalDistFromPlayers(DraculaView dv, PlaceId p, State *s) {
+	for (int i =0; i < NUM_PLAYERS; i++) {
+		s->distFromPlayers[i] = calculateDistance(dv, p, PLAYER_DRACULA);
+	}
 }
 // Takes in a possible positions and evaluates their state.
 // Returns a list. 
-State * evaluatePositionState(PlaceId *positions) {
-	return NULL;
+State * evaluatePositionState(PlaceId *positions, Player currPlayer,State *rootState, int numPositions) {
+	// From the root state, generate possible states.
+	State *s = malloc(sizeof(struct _state) * numPositions); 
+	for (int i =0; i < numPositions; i++) {
+		s[i] = *rootState; 
+		s[i].whereIsPlayer[currPlayer] = positions[i];
+	}
+	return s;
 }
-// Returns best state. 
-State minimaxHelper(DraculaView dv, State rootState, int currDepth, bool isMaximising) {
+
+void appendToTrail(PlaceId * trail) {
+
+}
+// Returns best move. 
+PlaceId minimaxHelper(DraculaView dv, State *rootState, int currDepth, bool isMaximising) {
 	// For the rootState, call the recursive function on its children
 	// And evaluate the max. 
 	int len = -1; 
 	PlaceId *possiblePositions = DvWhereCanIGo(dv, &len);
-	State * s = evaluatePositionState(possiblePositions);
-	int maxScore = -1e9; // Very smoll number. 
-	int index = -1; 
+	//State * s = evaluatePositionState(possiblePositions, PLAYER_DRACULA, rootState, len);
+	int maxScore = INT_MIN; // Very smoll number. 
+	PlaceId retValue; 
 	for (int i =0; i < len; i++) {
-		int score = minimax(dv, s[i], currDepth+1, !isMaximising); // switch isMaximising.
+		State s = *rootState;
+		
+		s.whereIsPlayer[PLAYER_DRACULA] = possiblePositions[i];
+		evalDistFromPlayers(dv, possiblePositions[i], &s); 
+		//maxScore = max(maxScore, minimax(dv, s, currDepth+1, !isMaximising)); // switch isMaximising.
+		int score = minimax(dv, s, currDepth+1, !isMaximising); // switch isMaximising.
 		if (score > maxScore) {
 			maxScore = score;
-			index = i; 
+			retValue = possiblePositions[i]; 
 		}
 	}
-	return s[index];
+	return retValue;
 }
+
 // Evaluate each substate and return a score.
 // Need to support Hunter States as well. 
 // Perhaps only consider distance for now? Hunter's goal is to minimize distance. 
 int minimax(DraculaView dv, State currState, int currDepth, bool isMaximising) {
 	if (currDepth >= MAX_DEPTH) return evalFunction(currState);
 	int len = -1; 
-	PlaceId *possiblePositions = DvWhereCanIGo(dv, &len);
-	State * s = evaluatePositionState(possiblePositions);
+	//PlaceId *possiblePositions = DvWhereCanIGo(dv, &len);
 	if (isMaximising) {
-		long long maxScore = -1e9; // very low number. 
+		int maxScore = INT_MIN; // very low number. 
+		PlaceId *possiblePositions = DvWhereCanIGo(dv, &len);
+		//State * s = evaluatePositionState(possiblePositions, PLAYER_DRACULA, &currState, len);
 		for (int i = 0; i < len; i++) {
-			maxScore = max(maxScore, minimax(dv, s[i], currDepth+1, !isMaximising)); // switch isMaximising.
+			State s = currState; 
+			s.whereIsPlayer[PLAYER_DRACULA] = possiblePositions[i];
+			evalDistFromPlayers(dv, possiblePositions[i], &s); 
+			maxScore = max(maxScore, minimax(dv, s, currDepth+1, !isMaximising)); // switch isMaximising.
 		}
-		assert(maxScore != -1e9);
+		assert(maxScore != INT_MIN);
 		return maxScore;
 	} else {
-		long long  minScore = 1e9; // very high number. 
-		for (int i =0; i < len; i++) {
-			minScore = min(minScore, minimax(dv, s[i], currDepth+1, !isMaximising));
+		//int minScore = 1e9; // very high number.
+		PlaceId minScores[NUM_PLAYERS-1] = {NOWHERE, NOWHERE, NOWHERE, NOWHERE}; 
+		State newState = currState;
+		int score = INT_MAX; 
+		for (int player =0; player < NUM_PLAYERS - 1; player++) {
+			PlaceId *possiblePositions = DvWhereCanIGo(dv, &len);
+			int global_min = INT_MAX; 
+			for (int currPosIndex =0; currPosIndex < len; currPosIndex++) {
+				int curr_min = calculateDistance(dv, possiblePositions[currPosIndex], currState.whereIsPlayer[PLAYER_DRACULA]);
+				/*
+				State s = currState; 
+				s.whereIsPlayer[i] = po
+				minScore = min(minScore, minimax(dv, s[i], currDepth+1, !isMaximising));
+				*/
+				if (curr_min < global_min) {
+					global_min = curr_min;
+					minScores[player] = possiblePositions[currPosIndex];
+				}
+			}
+			score = min(score, global_min);
+			newState.whereIsPlayer[player] = minScores[player];
 		}
-		assert(minScore != 1e9);
-		return minScore; 
+		minimax(dv, newState, currDepth+1, !isMaximising);
+		//assert(minScore != 1e9);
+		// Currently just returns the smallest distance between them
+		return score; 
 	}
 }
